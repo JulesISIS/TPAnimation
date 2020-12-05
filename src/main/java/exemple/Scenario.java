50f52df1c7a6356a70df2894185bc4a0428cd5c2
mjule <mjule@Gemini>
2020-12-05 21:54:13 +0100

    Rendu final V2

--- src/main/java/exemple/Scenario.java
+++ src/main/java/exemple/Scenario.java
@@ -9,14 +9,14 @@ import java.util.ArrayList;
  */
 public class Scenario {
 
-    Bandeau monBandeau = new Bandeau();
-    public ArrayList<Effet> myEffets = new ArrayList<>();
+    private Bandeau monBandeau = new Bandeau();
+    private ArrayList<Effet> myEffets = new ArrayList<>();
 
     public void ajouterEffet(Effet effet) {
         myEffets.add(effet);
     }
 
-    public void jouerScenario(Bandeau bandeau) {
+    public void jouerScenario() {
         for (int i = 0; i < myEffets.size(); i++) {
             myEffets.get(i).executer();
         }
